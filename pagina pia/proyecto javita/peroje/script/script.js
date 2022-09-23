f39f1048c42b3408bdf5850d2260219cebc55042
sofinista <105441648+sofinista@users.noreply.github.com>
2022-09-22 19:52:58 -0500

    Add files via upload

---
+++ pagina pia/proyecto javita/peroje/script/script.js
@@ -0,0 +1,65 @@
+import {datos} from '../data/datos.js';
+const items = document.getElementById('items');
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment()
+let like=[];
+let dislike=[];
+
+document.addEventListener('DOMContentLoaded',()=>{
+    loadData(datos)
+}
+)
+
+const loadData = datos =>{
+    datos.forEach(Flowers =>{
+        const{id,name,img} = Flowers;
+        templateCard.querySelector('h5').textContent = name
+        templateCard.querySelector('img').setAttribute('src',img);
+        templateCard.querySelector('.btn').dataset.id = id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment);
+}
+
+items.addEventListener('click', e =>{
+    addLike(e);
+})
+
+const addLike = e => {
+    if(e.target.classList.contains('btn-dark')) {
+        setLike(e.target.parentElement);
+    } else if (e.target.classList.contains('btn-light')){
+        setDislike(e.target.parentElement)
+    }
+}
+
+const setLike = objeto =>{
+    const boton = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        cantidad: 0
+    }
+    if(like.hasOwnProperty(boton.id)) {
+        boton.cantidad = like [boton.id].cantidad + 1;
+        objeto.querySelector('#like').textContent = boton.cantidad;
+    }
+    like[boton.id] = {...boton};
+
+    console.log(like[boton.id]);
+}
+
+const setDislike = objeto =>{
+    const boton = {
+        id: objeto.querySelector('.btn').dataset.id,
+        cantidad: 0
+    }
+    if(dislike.hasOwnProperty(boton.id)) {
+        boton.cantidad = dislike [boton.id].cantidad + 1;
+        objeto.querySelector('#dislike').textContent = boton.cantidad;
+    }
+    dislike[boton.id] = {...boton};
+
+    console.log(dislike[boton.id]);
+}
+
+
